8262e95a5f49ef7f4c3ea991f6a083afc813b501
Vedran Stojnović <phidrho@gmail.com>
2023-03-22 17:38:54 +0100

    prva verzija

---
+++ js/serviceWorker.js
@@ -0,0 +1,23 @@
+var myCacheName = "myPWACache";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers#install_and_activate_populating_your_cache
+const addResourcesToCache = async (resources) => {
+    console.log("Add to cache...");
+    const cache = await caches.open(myCacheName); // ovdje smo upisali našu gore definiranu varijablu
+    await cache.addAll(resources);
+  };
+  
+  self.addEventListener("install", (event) => {
+    console.log("Installing...");
+    event.waitUntil(
+      addResourcesToCache([
+        "/",
+        "/index.html",
+        "/css/style.css",
+        "/js/app.js",
+        "/media/icons/32.png",
+        "/media/icons/512.png"
+        ])
+    );
+  });
+  
